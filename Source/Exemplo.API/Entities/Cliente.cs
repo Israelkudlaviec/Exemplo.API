b077197d2279e0f4d42514432d3b21e4fef64c33
Israel Kudlaviec <i.kudlaviec@cinq.com.br>
2019-04-16 15:30:22 -0300

    Adicionar arquivos de projeto.

---
+++ Source/Exemplo.API/Entities/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo.API.Entities
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
